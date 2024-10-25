9d84da4bb3d0bb2af8a764c8f385d628c6ab799b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-10-24 21:31:32 -0300

    Novas funcionalidades clicks e listas

--- js/main.js
+++ js/main.js
@@ -6,15 +6,22 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-
   // Template String
   const idAudio = `#som_${instrumento}`;
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
 
-  contador++;
+  tecla.onkeydown = function (evento) {
+    if (evento.code == "Space" || evento.code == "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
